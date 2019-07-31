7bea81f65e5126557310cc84122a7d65d1f189c4
JohnWillian <43693831+JohnWillian@users.noreply.github.com>
2019-07-31 13:01:09 +0800

    Update detail.js

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -108,55 +108,7 @@ Page({
     
     }
   },
-  //微信支付
-  // order(){
-  //   wx.cloud.callFunction({
-  //     name:'emall-pay',
-  //     data:{
-  //       type: 'unifiedorder',
-  //       data:{
-  //         goodId: this.data.id
-  //       }
-  //     },
-  //     success:res=>{
-  //       const data = result.data
-
-  //       // 再次签名
-  //       wx.cloud.callFunction({
-  //         name: 'emall-pay',
-  //         data: {
-  //           type: 'orderquery',
-  //           data: {
-  //             out_trade_no: result.result.data.out_trade_no
-  //           }
-  //         },
-  //         success:queryRet=>{
-  //           const{
-  //             time_stamp,
-  //             nonce_str,
-  //             sign,
-  //             prepay_id,
-  //             body,
-  //             total_fee
-  //           }=queryRet.result.data
-
-  //           // 拉起微信支付
-  //           wx.requestPayment({
-  //             timeStamp: 'time_stamp',
-  //             nonceStr: 'nonce_str',
-  //             package: 'prepay_id=${prepay_id}',
-  //             signType: 'MD5',
-  //             paySign: 'sign',
-  //             success(){
-  //               wx.hideLoading()
-  //             }
-  //           })
-  //         }
-  //       })
-  //     }
-  //   })
-  // },
-
+  
   onLoad: function (options) {
     
    this.setData({
